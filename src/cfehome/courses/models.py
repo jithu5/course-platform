9d5a1105a58847df428f1728cb35d93426b2c325
jithu5 <abijithr202@gmail.com>
2025-04-13 01:30:40 +0530

    initial commit with base design of course model

---
+++ src/cfehome/courses/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = 'publish', 'Published'
+    COMING_SOON = 'soon', 'Coming Soon'
+    DRAFT = 'draft', 'Draft'
+
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = 'anyone', 'Anyone'
+    EMAIL_REQUIRED = 'email', 'Email Required'
+
+# course model 
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to='courses/')
+    access = models.CharField(max_length=10, choices=AccessRequirement.choices, default=AccessRequirement.ANYONE)
+    status = models.CharField(max_length=10,choices=PublishStatus.choices,default=PublishStatus.DRAFT)
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
