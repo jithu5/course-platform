37643bbc9c29936448c31bbed2673c4ae1a07544
jithu5 <abijithr202@gmail.com>
2025-04-13 01:43:13 +0530

    remove sqlite from git

--- src/cfehome/courses/models.py
+++ src/cfehome/courses/models.py
@@ -12,15 +12,18 @@ class AccessRequirement(models.TextChoices):
     ANYONE = 'anyone', 'Anyone'
     EMAIL_REQUIRED = 'email', 'Email Required'
 
+def handle_upload(instance,filename):
+    return f"{filename}"
+
 # course model 
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     publish_date = models.DateTimeField(auto_now_add=True)
-    # image = models.ImageField(upload_to='courses/')
+    image = models.ImageField(upload_to=handle_upload,blank=True,null=True)
     access = models.CharField(max_length=10, choices=AccessRequirement.choices, default=AccessRequirement.ANYONE)
     status = models.CharField(max_length=10,choices=PublishStatus.choices,default=PublishStatus.DRAFT)
 
-    @property
+    @property 
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
