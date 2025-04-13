e8f84b70e03a0e96fccd56b00b7da20b7723d393
jithu5 <abijithr202@gmail.com>
2025-04-13 15:13:04 +0530

    implement cloudinary upload function

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course
 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['title','description','image','access','status','display_image'],
+    list_display = ['title','description','image','access','status']
+    list_filter = ['access','status']
+    readonly_fields=['display_image']
+
+
+    def display_image(self,obj,*args,**kwargs):
+        print(obj)
+
 admin.site.register(Course)
--- src/helpers/_cloudinary.py
+++ src/helpers/_cloudinary.py
@@ -11,5 +11,5 @@ def cloudinary_init():
         cloud_name = CLOUDINARY_CLOUD_NAME, 
         api_key = CLOUDINARY_API_KEY, 
         api_secret = CLOUDINARY_API_SECRET, # Click 'View API Keys' above to copy your API secret
-        secure=True
+        secure=True,
     )
